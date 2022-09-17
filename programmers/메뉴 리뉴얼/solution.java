cfdc4ea8b3c90fcc02d4bd6f5d5545dd1b6d94f9
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-09-17 17:51:56 +0900

    uploaded at 9/17/2022, 5:51:56 PM

---
+++ programmers/메뉴 리뉴얼/solution.java
@@ -0,0 +1,53 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+import java.util.*;
+import java.util.Map.Entry;
+
+class Solution {
+    // 각 order 의 character 들이 오름차순으로 주어지는 것은 아니다 ! 문자열을 생성할 때면 사전 순인 문자열이 생성 될 수 있도록 처리 필요
+    private int[] max = new int[11]; // 각 개수 당 -> max 등장횟수를 저장하는 배열
+
+    private Map<String, Integer> map = new HashMap<>();
+
+    private StringBuilder sb = new StringBuilder("");
+
+    public String[] solution(String[] orders, int[] course) {
+        for (int c : course) {
+            for (String order : orders) {
+                find(order, c, 0, 0);
+            }
+        }
+
+        List<String> list = new ArrayList<>(orders.length);
+
+        for (Entry<String, Integer> e : map.entrySet()) {
+            if (e.getValue() >= 2 && e.getValue() == max[e.getKey().length()]) {
+                list.add(e.getKey());
+            }
+        }
+        
+        Collections.sort(list);
+
+        return list.toArray(new String[]{});
+    }
+    
+    private void find(String order, int c, int cnt, int cur) {
+        if (cnt == c) {
+            char[] chars = sb.toString().toCharArray();
+            Arrays.sort(chars);
+            String str = String.valueOf(chars);
+
+            int numb = map.getOrDefault(str, 0) + 1;
+            map.put(str, numb);
+
+            max[c] = Math.max(max[c], numb); // max update
+            return;
+        }
+
+        for (int i = cur; i <= order.length() - (c - cnt); i++) {
+            sb.append(order.charAt(i));
+            find(order, c, cnt + 1, i + 1);
+            sb.delete(cnt, cnt + 1);
+        }
+    }
+}
