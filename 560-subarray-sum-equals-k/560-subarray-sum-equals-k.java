73edb4b63715fc309f53abc8c41e2e8251c033a7
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-10-02 14:43:52 +0900

    Time: 2222 ms (6.41%), Space: 43.7 MB (99.86%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -2,7 +2,7 @@ class Solution {
     private int cnt = 0;
     public int subarraySum(int[] nums, int k) {
         // 합이 k 인 subarray(-> 연속한 원소들로 이루어져있음) 의 개수를 리턴하라
-        return brute(nums, k);
+        return accum(nums,k);
     }
     
     // brute force 로 문제 풀기  
@@ -28,8 +28,24 @@ class Solution {
         return cnt;
     }
     
-    // dynamic programming 
-    private void dp(int[] nums, int k) {
+    // accumulated sum  
+    private int accum(int[] nums, int k) {
+        // to make sum, time complexity O(n) - sum[i + 1 ] = sum[i]  + nums[i + 1] -> to make it easier, init sum[0] = 0,and make sum array as array which size is nums.length + 1
+        int cnt = 0;
+        
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        
+        for(int i = 0; i < nums.length; i++) sum[i + 1] = sum[i] + nums[i];
+        
+        // sum of (i, j)  -> sum[j]  - sum[i] 
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j <= nums.length; j++) {
+                if(sum[j] - sum[i] == k) cnt++;
+            }
+        }
+        
+        return cnt;
         
     }
 }
