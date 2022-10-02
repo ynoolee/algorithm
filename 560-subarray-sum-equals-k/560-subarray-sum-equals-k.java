6f8b39f3022da61c4a57e6bf0a47500d51cfc5c3
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-10-02 15:55:04 +0900

    Time: 60 ms (34.62%), Space: 68.4 MB (15.86%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -2,7 +2,7 @@ class Solution {
     private int cnt = 0;
     public int subarraySum(int[] nums, int k) {
         // 합이 k 인 subarray(-> 연속한 원소들로 이루어져있음) 의 개수를 리턴하라
-        return accum(nums,k);
+        return usingAccumAndMap(nums, k);
     }
     
     // brute force 로 문제 풀기  
@@ -47,5 +47,28 @@ class Solution {
         
         return cnt;
         
+    }
+    
+    // using hashmap
+    
+    private int usingAccumAndMap(int[] nums, int k) {
+        int cnt = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        
+        for(int i = 0; i < nums.length; i++) sum[i + 1] = sum[i] + nums[i];
+        
+        for(int end = 0; end <= nums.length; end++) {
+            // sum[end] - k 인 sum[start] 가 map 에 존재하는가? -> 존재한다면 그 개수만큼 count 횟수를 증가시키면 된다 -> 정확한 start 를 알 수는 없지만, map 에 저장된 sum[start] 의 발생횟수들은 모두 end 이전의 index 들에 해당한다 
+            if(map.containsKey(sum[end] - k)) {
+                cnt += map.get(sum[end] - k);   
+            }
+            
+            map.put(sum[end], map.getOrDefault(sum[end] ,0) + 1);
+        }
+        
+        return cnt;
     }
 }
