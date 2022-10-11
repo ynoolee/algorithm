6f8214dc8212e0c5360bb800732aa284a86ebfdd
ynoolee <heerarea129@gmail.com>
2022-10-11 16:44:48 +0900

    chore : [level 1] Title: 소수 찾기 - 주석 수정
    - 다른 문제 풀던 주석이 남아 있어 이를 제거함
    시간 복잡도 주석을 수정함

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -4,7 +4,7 @@ class Solution {
     /**
      * N 개의 수 중에서 소수가 "몇 개 " 인지 찾는 문제
      * 1개의 수가 소수인지를 판별하는 문제가 아니다
-     * 이 문제의 경우 N 은 100이하의 수이기 때문에 내가 정리한 소수 판별 방법 세 가지 모두 정답을 도출 할 수 있다.
+     * 이 문제의 경우 N 은 100 만이하의 수이기 때문에 내가 정리한 소수 판별 방법 세 가지 중 naive 한 방식으로는 답 도출 불가능(시간초과))
      *
      * */
     public int solution(int n) {
@@ -13,7 +13,7 @@ class Solution {
     }
 
 
-    // O(n)
+    // O(n^2)
     private int naive(int n){
         int cnt = 0;
 
@@ -33,7 +33,7 @@ class Solution {
         return cnt;
     }
 
-    // O(루트N)
+    // O(N루트N)
     private int divisor(int n) {
         int cnt = 0;
 
@@ -53,7 +53,7 @@ class Solution {
         return cnt;
     }
 
-    // O(Nlog(logN))
+    // O(Nlog(logN)) 따라서 이 방식은 N 이 커도 평균적으로 적은 시간이 걸린다
     private int eratos(int n) {
         boolean[] visit = new boolean[n + 1]; // index 가 숫자이기 때문에 n + 1 크기여야함
 
