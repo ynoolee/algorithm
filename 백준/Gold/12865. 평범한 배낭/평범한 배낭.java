a53d4cad8ffbf97c211040a6035333e05381d293
ynoolee <53856184+ynoolee@users.noreply.github.com>
2023-03-19 16:11:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 54288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,66 +1,58 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
-
-    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    private StringTokenizer st;
-
-    private int[][] dp;
-
-    private int[] v;
-
-    private int[] w;
-
-    private int n, k;
-
-    private void run() throws IOException {
-        st = new StringTokenizer(br.readLine());
+    public static int n;
+    public static int k;
+    public static int[][] dp ;
+    // [n+1][k+1] 로 해야함 . 왜냐하면, 현재 무게 j 에서 - w[i]를 뺏을 때 0이 나오는 경우가 있을 수 있거든 .
+    // 따라서 해당 경우를 따로 다뤄줘야하므로, 0으로 채워진 column이 맨 좌측에 존재한다고 생각
+    public static int[][] items ;
+    //public static ArrayList<int[]> items = new ArrayList<>(101);
+    // items.get(i)[0]는 weight, [1]은 value
+    public static void Setting() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
-        dp = new int[n][100001]; // dp 배열 생성
-
-        v = new int[n]; // 가치 배열
-        w = new int[n]; // 무게 배열
-
-        for (int i = 0; i < n; i++) {
+        dp = new int[n+1][k+1];
+        items = new int[n][2];
+        int w=0,v=0;
+        // n 개의 물건에 대하여
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
+            items[i][0]  = Integer.parseInt(st.nextToken());
+            items[i][1]  = Integer.parseInt(st.nextToken());
+            //items.add(i,new int[]{w,v});
 
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
         }
-
-        int result = recur(0, 0);
-        System.out.println(result);
     }
-
-    // i 번째 물건을 선택하러 왔을 당시까지의 무게
-    
-    private int recur(int i, int curW) {
-        if( i == n ) return 0;
-        if (dp[i][curW] != 0) {
-            return dp[i][curW];
+    public static void dp(){
+        for(int j=1;j<=k;j++){
+            // item 저장은 0부터 해서 n개를 해 놓았지만,
+            // dp에서는 i가 0 인 것을 1 인 것으로 다루도록 한다 .
+            // 왜냐하면, 첫 번 째 item에 대해서도  dp[i][j]= dp [i-1][j]를 할 수가 있어야 하기 때문.
+            for(int i=0;i<n;i++){
+                if(items[i][0]<=j){
+                    // i 번째 item을 선택하는 것이 가능한 경우
+                    dp[i+1][j]= Math.max(dp[i][j],dp[i][j-items[i][0]]+items[i][1]);
+                }else{
+                    // i 번째 item을 선택하는 것이 불가능한 경우
+                    dp[i+1][j] = dp[i][j];
+                }
+            }
         }
-
-        int c1 = 0;
-        // 포함
-        if (curW + w[i] <= k) {
-            c1 = recur(i + 1, curW + w[i]) + v[i];
-        }
-        // 미 포함
-        int c2 = recur(i + 1, curW);
-
-        return dp[i][curW] = Math.max(c1, c2);
+        // 따라서 최대값은 무조건 dp[n][k]에 저장되어있다.
+        System.out.println(dp[n][k]);
     }
-
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        main.run();
+        Setting();
+        dp();
     }
 
 }
